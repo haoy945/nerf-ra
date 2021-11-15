c49ddd7b2fc66a437eaad25de07a7e831b67ef8b
hao.yang <sundayyoung@mail.ustc.edu.cn>
2021-11-15 18:49:05 +0800

    add embedder.

---
+++ nerf/layers/embedder.py
@@ -0,0 +1,24 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+def embedding(x, L):
+    freq = 2 ** torch.arange(L) * math.pi
+    freq = x[..., None] * freq
+    return torch.cat((freq.sin(), freq.cos()), dim=-1).flatten(x.dim() - 1)
+
+
+class Embedder(nn.Module):
+    def __init__(self, dim_position, dim_direction) -> None:
+        super().__init__()
+        self.dim_pos = dim_position
+        self.dim_dir = dim_direction
+
+    def forward(self, position, direction):
+        pos_embed = embedding(position, self.dim_pos)
+        dir_embed = embedding(direction, self.dim_dir)
+
+        return pos_embed, dir_embed
+
